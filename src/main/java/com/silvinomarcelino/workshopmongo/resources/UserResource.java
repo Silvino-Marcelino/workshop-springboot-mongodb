02e5190090c8510d27d5ad4166ed54fb01897ed4
Silvino Marcelino <silvinojll@gmail.com>
2024-09-05 20:26:32 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/silvinomarcelino/workshopmongo/resources/UserResource.java
+++ src/main/java/com/silvinomarcelino/workshopmongo/resources/UserResource.java
@@ -1,26 +1,24 @@
 package com.silvinomarcelino.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.silvinomarcelino.workshopmongo.domain.User;
+import com.silvinomarcelino.workshopmongo.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserServices services;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		List<User> list = new ArrayList<>();
-		User maria = new User("1", "Maria", "maria@gmail.com");
-		User joao = new User("2", "Joao", "Joao@gmail.com");
-		list.addAll(Arrays.asList(maria, joao));
+		List<User> list = services.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/silvinomarcelino/workshopmongo/services/UserServices.java
+++ src/main/java/com/silvinomarcelino/workshopmongo/services/UserServices.java
@@ -1,7 +1,18 @@
 package com.silvinomarcelino.workshopmongo.services;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.silvinomarcelino.workshopmongo.domain.User;
+import com.silvinomarcelino.workshopmongo.repository.UserRepository;
+
+@Service
 public class UserServices {
 
+	@Autowired
+	private UserRepository repository;
 	
-	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
 }
